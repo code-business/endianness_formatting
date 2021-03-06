49c43ba7c0bd8b9b84d129e6e982a0a4fc8b9fb1
gautam031098 <gomzi98@gmail.com>
2021-03-06 16:30:51 +0530

    changes

--- test-directory/test.js
+++ test-directory/test.js
@@ -1,5 +1,5 @@
 const { getFormattedEndianness } = require("hex_formatting");
-let hexString = "1326485b83a2742b1";
+let hexString = "1326485b83a2742b";
 let endianness = "Little Endian";
 
 let result = getFormattedEndianness(hexString, endianness);
