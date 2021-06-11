2739e335a0fd8294919f8f6df29f4efb0d2beace
Kuldeep Mane <kpmane84@gmail.com>
2021-06-11 14:36:47 +0530

    endianess format change

--- index.js
+++ index.js
@@ -30,17 +30,22 @@ const getFormattedEndianness = (hexString, endianness) => {
         return hexString;
     }
   }
-  else {
+  else if (hexArray.length === 8) {
+    switch (endianness) {
+      case "Mid-Little Endian":
+        mapper = [6, 7, 4, 5, 2, 3, 0, 1];
+        return mapper.map((i) => hexArray[i]).join("");
+      case "Little Endian":
+        mapper = [7, 6, 5, 4, 3, 2, 1, 0];
+        return mapper.map((i) => hexArray[i]).join("");
+      case "Mid-Big Endian":
+        mapper = [5, 4, 7, 6, 1, 0, 3, 2];
+        return mapper.map((i) => hexArray[i]).join("");
+      default:
+        return hexString;
+    }
+  } else {
     return hexString
-    // switch (endianness) {
-    //   // case "Mid-Little Endian":
-    //   //   mapper = [1, 0];
-    //   //   return mapper.map((i) => hexArray[i]).join("");
-
-    //   //For any other case(Eg. Big Endian), we return the string as it is
-    //   default:
-    //     return hexString;
-    // }
   }
 };
 
